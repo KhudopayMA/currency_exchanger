87b35041c21eed62db65bed98eb4189a9695124d
OldSnakeEater <mihail.hudopay@mail.ru>
2025-07-26 20:25:27 +0300

    добавил cors

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import logging.config
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.controller import currencies_controller, exchange_rates_controller
 from logger_config import LOGGING_CONFIG
@@ -11,8 +13,23 @@ logging.config.dictConfig(LOGGING_CONFIG)
 
 app = FastAPI()
 
+origins = [
+    'http://localhost:8080',
+    'http://0.0.0.0:8080'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.mount('/static', StaticFiles(directory='static'), name='static')
+
 app.include_router(currencies_controller.router)
 app.include_router(exchange_rates_controller.router)
 
 if __name__ == "__main__":
-    uvicorn.run(app)
+    uvicorn.run(app, host='0.0.0.0', port=8080)
